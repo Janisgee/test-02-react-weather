cf35535eb40a597e34ef0e462aecff94d87c2065
Janisgee <100589152+Janisgee@users.noreply.github.com>
2022-07-19 20:21:13 +0800

    Finalized

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,32 @@ function App() {
         <WeatherApp />
       </div>
       <div className="open-sourced">
-        This project was coded by Janis Chan and is open-sourced on GitHub and
-        hosted on Netlify.
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/jc-janis-chan"
+          title="Janis LinkedIn Profile"
+          target="_blank"
+        >
+          Janis Chan
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Janisgee/test-02-react-weather"
+          title="Weather App Github Source Code"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and
+        <a
+          href="https://app.netlify.com/teams/janisgee/overview"
+          title="Netlify Profile"
+          target="_blank"
+        >
+          {" "}
+          hosted on Netlify
+        </a>
+        .
       </div>
     </div>
   );
